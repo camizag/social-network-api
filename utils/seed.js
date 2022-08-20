ab11a761da28d1109d767cd239668753d16bf8f4
Camila Zagal <camilazags@gmail.com>
2022-08-20 08:14:31 -0500

    fixed typos

--- utils/seed.js
+++ utils/seed.js
@@ -1,17 +1,18 @@
 const connection = require('./../config/connection');
 const seedUsers = require('./userSeed');
 const seedThoughts = require('./commentSeed');
-const { User, Thought } = require('./../models');
+const { User, Comment } = require('./../models');
+const seedComment = require('./commentSeed');
 
 const seedData = async () => {
     await connection();
     await User.deleteMany();
     console.log('\n----- USER COLLECTION HAS BEEN WIPED! -----');
-    await Thought.deleteMany();
+    await Comment.deleteMany();
     console.log('\n----- THOUGHT COLLECTION HAS BEEN WIPED! -----');
     await seedUsers();
     console.log('\n----- USERS SEEDED SUCCESSFULLY! -----');
-    await seedThoughts();
+    await seedComment();
     console.log('\n----- THOUGHTS SEEDED SUCCESSFULLY! -----\n');
     process.exit();
 }
